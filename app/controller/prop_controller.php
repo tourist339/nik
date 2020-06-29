7aca3dae9f29d7f489e6c8d3042b0a8eef89942f
kartikkapoor <kak110@usask.ca>
2020-06-28 21:31:08 -0600

    prop_controller.php: Going with checking params in the same index function to load different pages

--- app/controller/prop_controller.php
+++ app/controller/prop_controller.php
@@ -3,32 +3,19 @@
 
 class prop_controller extends Controller
 {
-    public function showall($location)
+    public function index($location="",$propname="",$id="")
     {
-        if (isset($location)) {
+        if ($location!="") {
+
             $cView = $this->createView('prop/showprops', ["title" => "LyfLy",
                 "scripts" => ["jquery-3.5.1.js"],
                 "stylesheets" => ["homepage.css", "main.css"],
                 "navbar" => "navbar.html"]);
             $cView->render();
-        } else {
+        }else {
             new e404_controller();
         }
     }
 
-    public function showone($location, $propname, $id)
-    {
 
-    }
-    public function __call($method, $arguments) {
-        if($method == 'index') {
-            echo "56";
-            if(count($arguments) == 1) {
-                 call_user_func_array(array($this,'showall'), $arguments);
-            }
-            else if(count($arguments) == 3) {
-                 call_user_func_array(array($this,'showone'), $arguments);
-            }
-        }
-    }
 }
